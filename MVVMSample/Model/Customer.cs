2df357db010dcaef391ebc528259061eef3968bc
Pragmateek <postmaster@pragmateek.com>
2014-07-28 00:32:21 +0200

    Setup of solution and first project

---
+++ MVVMSample/Model/Customer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMSample.Model
+{
+    public class Customer : INotifyPropertyChanged
+    {
+        private string firstName;
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (value != firstName)
+                {
+                    firstName = value;
+
+                    PropertyChanged(this, new PropertyChangedEventArgs("FirstName"));
+                }
+            }
+        }
+
+        private string lastName;
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (value != lastName)
+                {
+                    lastName = value;
+
+                    PropertyChanged(this, new PropertyChangedEventArgs("LastName"));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+    }
+}
