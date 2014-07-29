daea2ab0d4af94ba1e9afc9403b1f48a2229c108
Pragmateek <postmaster@pragmateek.com>
2014-07-29 17:42:15 +0200

    Move fruits list to assets project and add a grouping sample

---
+++ Assets/Assets.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets
+{
+    public static class Assets
+    {
+        public static IEnumerable<string> Fruits
+        {
+            get
+            {
+                return new[]
+                {
+                    "Apple",
+                    "Apricot",
+                    "Avocado",
+                    "Banana",
+                    "Breadfruit",
+                    "Bilberry",
+                    "Blackberry",
+                    "Blackcurrant",
+                    "Blueberry",
+                    "Boysenberry",
+                    "Cantaloupe",
+                    "Currant",
+                    "Cherry",
+                    "Cherimoya",
+                    "Cloudberry",
+                    "Coconut",
+                    "Cranberry",
+                    "Cucumber",
+                    "Damson",
+                    "Date",
+                    "Dragonfruit",
+                    "Durian",
+                    "Eggplant",
+                    "Elderberry",
+                    "Feijoa",
+                    "Fig",
+                    "Goji berry",
+                    "Gooseberry",
+                    "Grape",
+                    "Raisin",
+                    "Grapefruit",
+                    "Guava",
+                    "Huckleberry",
+                    "Honeydew",
+                    "Jackfruit",
+                    "Jambul",
+                    "Jujube",
+                    "Kiwi fruit",
+                    "Kumquat",
+                    "Lemon",
+                    "Lime",
+                    "Loquat",
+                    "Lychee",
+                    "Mango",
+                    "Marion berry",
+                    "Melon",
+                    "Cantaloupe",
+                    "Honeydew",
+                    "Watermelon",
+                    "Rock melon",
+                    "Miracle fruit",
+                    "Mulberry",
+                    "Nectarine",
+                    "Nut",
+                    "Olive",
+                    "Orange",
+                    "Clementine",
+                    "Mandarine",
+                    "Blood Orange",
+                    "Tangerine",
+                    "Papaya",
+                    "Passionfruit",
+                    "Peach",
+                    "Pepper",
+                    "Pear",
+                    "Persimmon",
+                    "Physalis",
+                    "Plum",
+                    "Pineapple",
+                    "Pomegranate",
+                    "Pomelo",
+                    "Purple Mangosteen",
+                    "Quince",
+                    "Raspberry",
+                    "Rambutan",
+                    "Redcurrant",
+                    "Salal berry",
+                    "Salmon berry",
+                    "Satsuma",
+                    "Star fruit",
+                    "Strawberry",
+                    "Tamarillo",
+                    "Tomato",
+                    "Ugli",
+                    "Watermelon"
+                };
+            }
+        }
+    }
+}
