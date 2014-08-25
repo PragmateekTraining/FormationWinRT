88a0ec30b669b52e9890505a61d7ae8ffe2722bc
Pragmateek <postmaster@pragmateek.com>
2014-08-25 23:41:31 +0200

    Add some data binding samples

---
+++ DataBindingSample/SpreadConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace DataBindingSample
+{
+    public class SpreadConverter : IValueConverter
+    {
+        public int Max { get; set; }
+
+        public SpreadConverter()
+        {
+            Max = -1;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            int? reference = parameter != null ? System.Convert.ToInt32(parameter) :
+                             Max != -1 ? Max :
+                             null as int?;
+
+            if (reference == null)
+                return DependencyProperty.UnsetValue;
+
+            return reference - System.Convert.ToInt64(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
