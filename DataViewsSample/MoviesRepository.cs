7996900ad5ad5b21ec49d904ea037cfa7489ac59
Pragmateek <postmaster@pragmateek.com>
2014-08-26 22:50:29 +0200

    Add FlipView and ListView samples

---
+++ DataViewsSample/MoviesRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataViewsSample
+{
+    public class MoviesRepository
+    {
+        public static IEnumerable<Movie> GetAllMovies()
+        {
+            return new[]
+            {
+                new Movie{ Title = "Die Hard", PosterURI = "/Assets/MoviesDB/Posters/DieHard.jpg" },
+                new Movie{ Title = "Die Hard 2", PosterURI = "/Assets/MoviesDB/Posters/DieHard2.jpg" },
+                new Movie{ Title = "Die Hard 3", PosterURI = "/Assets/MoviesDB/Posters/DieHard3.jpg" },
+                new Movie{ Title = "Die Hard 4", PosterURI = "/Assets/MoviesDB/Posters/DieHard4.jpg" },
+                new Movie{ Title = "Die Hard 5", PosterURI = "/Assets/MoviesDB/Posters/DieHard5.jpg" }
+            };
+        }
+    }
+}
---
+++ DataViewsSample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataViewsSample
+{
+    public class MainPageViewModel
+    {
+        public ObservableCollection<Movie> Movies { get; set; }
+
+        public MainPageViewModel()
+        {
+            this.Movies = new ObservableCollection<Movie>(MoviesRepository.GetAllMovies());
+        }
+    }
+}
