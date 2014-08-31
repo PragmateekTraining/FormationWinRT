0f36d75c00304a5cfa3c4b980f2a209797d263a8
Pragmateek <postmaster@pragmateek.com>
2014-08-31 23:56:56 +0200

    Add flyouts + MVVM sample

---
+++ FlyoutsSample/BinarySwitchConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace FlyoutsSample
+{
+    public class BinarySwitchConverter : DependencyObject, IValueConverter
+    {
+        public static readonly DependencyProperty ValueIfTrueProperty = DependencyProperty.Register("ValueIfTrue", typeof(object), typeof(BinarySwitchConverter), null);
+
+        public object ValueIfTrue
+        {
+            get
+            {
+                return this.GetValue(ValueIfTrueProperty);
+            }
+            set
+            {
+                this.SetValue(ValueIfTrueProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty ValueIfFalseProperty = DependencyProperty.Register("ValueIfFalse", typeof(object), typeof(BinarySwitchConverter), null);
+
+        public object ValueIfFalse
+        {
+            get
+            {
+                return this.GetValue(ValueIfFalseProperty);
+            }
+            set
+            {
+                this.SetValue(ValueIfFalseProperty, value);
+            }
+        }
+
+        public object Convert(object value, Type targetType, object parameter, string culture)
+        {
+            return (bool)value ? ValueIfTrue : ValueIfFalse;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FlyoutsSample/MainViewModel.cs
@@ -0,0 +1,106 @@
+﻿using FlyoutsSample.Common;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FlyoutsSample
+{
+    public abstract class NotifyPropertyChangedBase : INotifyPropertyChanged
+    {
+        protected void UpdateAndNotify<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(value, field))
+            {
+                field = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+    }
+
+    public class CartItem : NotifyPropertyChangedBase
+    {
+        private Movie movie;
+        public Movie Movie
+        {
+            get { return movie; }
+            set { UpdateAndNotify(ref movie, value); }
+        }
+
+        private uint quantity;
+        public uint Quantity
+        {
+            get { return quantity; }
+            set { UpdateAndNotify(ref quantity, value); }
+        }
+
+        private bool isAGift;
+        public bool IsAGift
+        {
+            get { return isAGift; }
+            set { UpdateAndNotify(ref isAGift, value); }
+        } 
+    }
+
+    public class MainViewModel : NotifyPropertyChangedBase
+    {
+        public ObservableCollection<Movie> AvailableMovies { get; set; }
+
+        public ObservableCollection<CartItem> CartItems { get; set; }
+
+        public ICommand AddToCartCommand { get; set; }
+
+        public ICommand RemoveOneFromCartCommand { get; set; }
+        public ICommand RemoveFromCartCommand { get; set; }
+
+        public MainViewModel()
+        {
+            AvailableMovies = new ObservableCollection<Movie>(MoviesRepository.Movies);
+
+            CartItems = new ObservableCollection<CartItem>();
+
+            AddToCartCommand = new RelayCommand(AddToCart);
+
+            RemoveOneFromCartCommand = new RelayCommand(RemoveOneFromCart);
+            RemoveFromCartCommand = new RelayCommand(RemoveFromCart);
+        }
+
+        private void AddToCart(object parameter)
+        {
+            Movie movie = (Movie)parameter;
+
+            CartItem cartItem = CartItems.SingleOrDefault(item => item.Movie == movie);
+
+            if (cartItem == null)
+            {
+                cartItem = new CartItem { Movie = movie, Quantity = 0 };
+
+                CartItems.Add(cartItem);
+            }
+
+            ++cartItem.Quantity;
+        }
+
+        private void RemoveFromCart(object parameter)
+        {
+            CartItem cartItem = (CartItem)parameter;
+
+            CartItems.Remove(cartItem);
+        }
+
+        private void RemoveOneFromCart(object parameter)
+        {
+            CartItem cartItem = (CartItem)parameter;
+
+            cartItem.Quantity = Math.Max(cartItem.Quantity - 1, 1);
+        }
+    }
+}
