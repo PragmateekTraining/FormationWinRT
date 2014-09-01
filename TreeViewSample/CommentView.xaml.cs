504b3d5cb2f30222078b5ef7310bae3a11422391
Pragmateek <postmaster@pragmateek.com>
2014-09-01 13:42:56 +0200

    Fake TreeView sample

---
+++ TreeViewSample/CommentView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace TreeViewSample
+{
+    public sealed partial class CommentView : UserControl
+    {
+        public Comment Comment
+        {
+            get { return (Comment)GetValue(CommentProperty); }
+            set { SetValue(CommentProperty, value); }
+        }
+
+        public static readonly DependencyProperty CommentProperty = DependencyProperty.Register("Comment", typeof(Comment), typeof(CommentView), new PropertyMetadata(null));        
+
+        public CommentView()
+        {
+            this.InitializeComponent();
+
+            root.DataContext = this;
+        }
+    }
+}
---
+++ TreeViewSample/MainViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeViewSample
+{
+    public class Comment
+    {
+        public string Text { get; set; }
+
+        public IEnumerable<Comment> Comments { get; set; }
+    }
+
+    public class MainViewModel
+    {
+        public IEnumerable<Comment> Comments { get; set; }
+
+        public MainViewModel()
+        {
+            Comments = new[]
+            {
+                new Comment
+                {
+                    Text = "root comment #1",
+                    Comments = new[]
+                    {
+                        new Comment
+                        {
+                            Text = "comment #1 #1",
+                            Comments = new[]
+                            {
+                                new Comment
+                                {
+                                    Text = "comment #1 #1 #1"
+                                }
+                            }
+                        },
+                        new Comment
+                        {
+                            Text = "comment #1 #2"
+                        }
+                    }
+                },
+                new Comment
+                {
+                    Text = "root comment #2",
+                    Comments = new[]
+                    {
+                        new Comment
+                        {
+                            Text = "comment #2 #1",
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
